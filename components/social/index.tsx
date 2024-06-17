e0c26ccf01e89e9a3deecbe4ca39a3154f4be1f2
Zlatko <iliev.zlatko@gmail.com>
2024-06-17 12:15:23 +0300

    add button variants

---
+++ components/social/index.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { FcGoogle } from 'react-icons/fc'
+import { Button } from '@/components/ui/button'
+import { PROVIDERS } from '@/consts/providers'
+import { DEFAULT_LOGIN_REDIRECT } from '@/consts/protectedRoutes'
+import { signIn } from 'next-auth/react'
+
+export const Social = ({ label }: { label?: string }) => {
+  return (
+    <div className='flex items-center gap-x-2 w-full'>
+      <Button
+        variant='primary-outline'
+        className='w-full gap-2 rounded-lg border-[#3C4B57]'
+        onClick={() =>
+          signIn(PROVIDERS.GOOGLE, {
+            callbackUrl: DEFAULT_LOGIN_REDIRECT,
+          })
+        }
+      >
+        <FcGoogle className='h-5 w-5' />
+        {label && <span className='text-[#3C4B57]'>{label}</span>}
+      </Button>
+    </div>
+  )
+}
