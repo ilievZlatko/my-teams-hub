2552548024962a7ff08ff9e5abfa4b35d97fde17
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-06-30 22:56:18 +0300

    changes to sidebar

--- components/side-bar-route/index.tsx
+++ components/side-bar-route/index.tsx
@@ -3,21 +3,25 @@
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@radix-ui/react-accordion";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import React, { useState } from "react";
 
-type SubRoute = {
+interface SubRoute {
   routeName: string;
-  subRoutes?: Array<SubRoute>;
-};
+  subRoutes?: SubRoute[];
+  url?: string;
+}
 
 type SideBarRouteProps = {
   image: string;
   routeName: string;
-  subRoutes?: Array<SubRoute>;
-  isOpen:boolean;
+  subRoutes?: SubRoute[];
+  isOpen: boolean;
+  url?: string
+  onToggle: () => void;
+  onRouteClick: (url?: string) => void; 
 };
 
-export const SideBarRoute = ({ image, routeName, subRoutes,isOpen }: SideBarRouteProps) => {
+export const SideBarRoute: React.FC<SideBarRouteProps> = ({ image, routeName, subRoutes, isOpen,url, onToggle, onRouteClick }) => {
   const [openAccordionItems, setOpenAccordionItems] = useState<string[]>([]);
 
   const toggleAccordionItem = (value: string) => {
@@ -33,13 +37,18 @@ export const SideBarRoute = ({ image, routeName, subRoutes,isOpen }: SideBarRout
   return (
     <div className="flex flex-col items-start w-full">
       <div className="flex items-center gap-6 cursor-pointer" onClick={() => toggleAccordionItem(routeName)}>
-        <Image src={image} alt={"side-bar-image"} width={24} height={24} />
-        <p className={`${isOpen ? "block" : "hidden"} mb-0 text-mth-silver-200 hover:text-mth-grey-blue-900 transition duration-300 ease-in-out`}>
+        <Image src={image} alt={"side-bar-image"} width={24} height={24} onClick={onToggle} />
+        <p
+          className={`${isOpen ? "block" : "hidden"} mb-0 text-mth-silver-200 hover:text-mth-grey-blue-900 transition duration-300 ease-in-out font-poppins`}
+          onClick={(e) => {
+            onRouteClick(url)}
+          } 
+        >
           {routeName}
         </p>
         {subRoutes && isOpen && (
           <div className="ml-3 pt-1">
-            {isAccordionItemOpen(routeName)  ? (
+            {isAccordionItemOpen(routeName) ? (
               <ChevronUp className="h-5 w-6 transition-transform duration-200 text-mth-silver-200" />
             ) : (
               <ChevronDown className="h-5 w-6 transition-transform duration-200 text-mth-silver-200" />
@@ -47,14 +56,17 @@ export const SideBarRoute = ({ image, routeName, subRoutes,isOpen }: SideBarRout
           </div>
         )}
       </div>
-      {subRoutes && (
-        <Accordion type="multiple"  className={`${isAccordionItemOpen(routeName) ? "block" : "hidden"} w-full pt-5`}>
+      {subRoutes && isOpen && (
+        <Accordion type="multiple" className={`${isAccordionItemOpen(routeName) ? "block" : "hidden"} w-full pt-5`}>
           {subRoutes.map((subRoute, index) => (
             <AccordionItem key={index} value={`${routeName}-${index}`}>
-              <AccordionTrigger className="flex justify-between mb-3 ml-6 text-mth-silver-200 hover:text-mth-grey-blue-900 transition duration-300 ease-in-out pl-6" onClick={() => toggleAccordionItem(`${routeName}-${index}`)}>
-                <p>{subRoute.routeName}</p>
+              <AccordionTrigger
+                className="flex justify-between mb-3 ml-6 text-mth-silver-200 hover:text-mth-grey-blue-900 transition duration-300 ease-in-out pl-6"
+                onClick={() => toggleAccordionItem(`${routeName}-${index}`)}
+              >
+                <p onClick={() => onRouteClick(subRoute.url)}>{subRoute.routeName}</p>
                 {subRoute.subRoutes && (
-                  <div className="ml-3 pt-1">
+                  <div className="ml-3">
                     {isAccordionItemOpen(`${routeName}-${index}`) ? (
                       <ChevronUp className="h-5 w-6 transition-transform duration-200 text-mth-silver-200" />
                     ) : (
@@ -66,7 +78,11 @@ export const SideBarRoute = ({ image, routeName, subRoutes,isOpen }: SideBarRout
               {subRoute.subRoutes && (
                 <AccordionContent>
                   {subRoute.subRoutes.map((nestedSubRoute, nestedIndex) => (
-                    <div key={nestedIndex} className="cursor-pointer mb-2 ml-8 text-mth-silver-200 hover:text-mth-grey-blue-900 transition duration-300 ease-in-out pl-12">
+                    <div
+                      key={nestedIndex}
+                      className="cursor-pointer mb-2 ml-8 text-mth-silver-200 hover:text-mth-grey-blue-900 transition duration-300 ease-in-out pl-12 text-sm font-normal font-poppins"
+                      onClick={() => onRouteClick(nestedSubRoute.url)}
+                    >
                       {nestedSubRoute.routeName}
                     </div>
                   ))}
--- components/side-bar/index.tsx
+++ components/side-bar/index.tsx
@@ -1,46 +1,75 @@
 "use client";
+
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { SideBarRoute } from "@/components/side-bar-route/index";
+import { useRouter } from "next/navigation";
+
+const routes = [
+  {
+    image: "/assets/images/overview.svg",
+    routeName: "Overview",
+    url: "/"
+  },
+  {
+    image: "/assets/images/management.svg",
+    routeName: "Management",
+    subRoutes: [
+      { routeName: "Organisation’s chart", url:"/organisations" },
+      {
+        routeName: "Teams",
+        subRoutes: [
+          { routeName: "View All Teams", url:"/view-all-teams" },
+          { routeName: "Add Team", url:"/add-team" },
+          { routeName: "Edit Team", url:"/edit-team" },
+        ],
+      },
+      {
+        routeName: "Users",
+        subRoutes: [
+          { routeName: "View All Users", url:"/all-users" },
+          { routeName: "Add User", url:"/add-user" },
+          { routeName: "Edit User", url:"/edit-user" },
+        ],
+      },
+    ],
+  },
+  {
+    image: "/assets/images/evaluation.svg",
+    routeName: "Evaluation",
+    // url: "/"
+  },
+];
 
 export const SideBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
 
-  const routes = [
-    { 
-      image: "/assets/images/overview.svg", 
-      routeName: "Overview",
-    },
-    {
-      image: "/assets/images/management.svg",
-      routeName: "Management",
-      subRoutes: [
-        { routeName: "Organisation’s chart" },
-        { routeName: "Teams",
-          subRoutes: [
-            { routeName: "View All Teams" },
-            { routeName: "Add Team" },
-            { routeName: "Edit Team" },
-          ],
-         },
-        {
-          routeName: "Users",
-          subRoutes: [
-            { routeName: "View All Users" },
-            { routeName: "Add User" },
-            { routeName: "Edit User" },
-          ],
-        },
-      ],
-    },
-    { 
-      image: "/assets/images/evaluation.svg", 
-      routeName: "Evaluation",
-    },
-  ];
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleRouteClick = (url?: string) => {
+    if (url) {
+      router.push(url);
+      // setIsOpen(false); 
+    }
+  };
+
 
   return (
-    <div className={`bg-mth-grey-blue-600 h-full transition-width duration-300 ease-in-out ${isOpen ? "w-80" : "w-22"}`}>
+    <div ref={sidebarRef} className={`bg-mth-grey-blue-600 h-full transition-width duration-300 ease-in-out ${isOpen ? "w-80" : "w-22"}`}>
       <div className="flex flex-col items-start space-y-10 px-8">
         <Image
           src={"/assets/images/hamburger.svg"}
@@ -51,7 +80,20 @@ export const SideBar = () => {
           onClick={() => setIsOpen(!isOpen)}
         />
         {routes.map((route, index) => (
-          <SideBarRoute key={index} image={route.image} routeName={route.routeName} subRoutes={route.subRoutes} isOpen={isOpen} />
+          <SideBarRoute
+            onToggle={() => {
+              if (!isOpen) {
+                setIsOpen(true)
+              }
+            }}
+            key={index}
+            image={route.image}
+            routeName={route.routeName}
+            subRoutes={route.subRoutes}
+            isOpen={isOpen}
+            url={route.url}
+      
+            onRouteClick={handleRouteClick} />
         ))}
       </div>
     </div>
