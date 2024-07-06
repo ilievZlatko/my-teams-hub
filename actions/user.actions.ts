261e9621b6241be7f9664182cdbf6f4b48253f6c
Denitsa Slavkova <deni9309@gmail.com>
2024-07-06 21:15:38 +0300

    Fix user.actions.ts to use 'fetch' instead of 'fetcher' wrapper

--- actions/user.actions.ts
+++ actions/user.actions.ts
@@ -1,19 +1,38 @@
 'use server'
 
 import routes from '@/api-routes'
-import { fetcher } from '@/lib/utils'
+import { auth } from '@/config/auth'
 import { IUser } from '@/types/user'
 
-export const getAllUsers = async () => {
+export async function getAllUsers(): Promise<IUser[] | { error: string }> {
   try {
-    const response = await fetcher<IUser[], null>({
-      url: routes.allUsers.get,
+    const session = await auth()
+    const headers = new Headers()
+
+    headers.append('Content-Type', 'application/json')
+    headers.append('Authorization', `Bearer ${session?.token?.access_token}`)
+
+    const url = `${process.env.API_BASE_URL}${routes.allUsers.get}`
+    const res = await fetch(url, {
       method: 'GET',
-      revalidateTags: ['teams']
+      headers,
+      next: {
+        tags: ['users'],
+      },
     })
 
-    return response
-  } catch (_error: unknown) {
+    const jsonResponse = await res.json()
+
+    if (jsonResponse?.errors && jsonResponse.errors?.length > 0) {
+      return { error: 'error_occurred_msg' }
+    }
+
+    return jsonResponse?.data ?? null
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    if (error?.message) {
+      return { error: error.message }
+    }
     return { error: 'error_occurred_msg' }
   }
 }
