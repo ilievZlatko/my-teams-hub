0066f8a261b30ac67d61de4faaecb65599d3a437
Zlatko <iliev.zlatko@gmail.com>
2024-07-06 12:31:21 +0300

    fix login redirect (#20)

--- app/[locale]/(auth)/login/page.tsx
+++ app/[locale]/(auth)/login/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
 import { LoginForm } from '@/components/login-form'
+import { redirect } from 'next/navigation'
+import { auth } from '@/config/auth'
 
-const LoginPage = () => {
+export default async function LoginPage() {
+  const session = await auth()
+
+  if (session && session.user) {
+    if (!session?.user?.activeOrg) return redirect('/select-org')
+    return redirect('/')
+  }
+
   return <LoginForm />
 }
-
-export default LoginPage
