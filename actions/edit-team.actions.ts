a4ea09de122732b2856faf5f8998cdf879588f06
Denitsa Slavkova <deni9309@gmail.com>
2024-07-06 20:16:49 +0300

    Fix edit-team.actions.ts Use basic "fetch" instead of "fetcher"

--- actions/edit-team.actions.ts
+++ actions/edit-team.actions.ts
@@ -4,29 +4,25 @@ import { revalidateTag } from 'next/cache'
 
 import routes from '@/api-routes'
 import { auth } from '@/config/auth'
-import { fetcher } from '@/lib/utils'
 import { EditTeamType, EditTeamSchema } from '@/schemas/edit-team.schema'
 import { PatchTeam, Team } from '@/types/team'
 
-export const editTeam = async (
+export async function editTeam(
   organizationId: string,
   teamId: string,
   values: EditTeamType,
-) => {
-  const session = await auth()
-  const validatedFields = EditTeamSchema.omit({ otherUsers: true }).safeParse(
-    values,
-  )
-
-  if (!session || !session.user.userId) {
-    return { error: 'unauthorized_request' }
-  }
-
-  if (!validatedFields.success) {
-    return { error: 'invalid_fields_msg' }
-  }
-
+): Promise<null | { error: string }> {
   try {
+    const session = await auth()
+
+    const validatedFields = EditTeamSchema.omit({ otherUsers: true }).safeParse(
+      values,
+    )
+
+    if (!validatedFields.success) {
+      return { error: 'invalid_fields_msg' }
+    }
+
     const { name, description, teamMembers } = validatedFields.data
     let members: { teamMemberId: string; email: string }[] = []
 
@@ -37,60 +33,105 @@ export const editTeam = async (
       }))
     }
 
-    const response = await fetcher<null, PatchTeam>({
-      url: routes.team.patch(organizationId, teamId),
+    const data: PatchTeam = { name, description, teamMembers: members }
+
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+    headers.append('Authorization', `Bearer ${session?.token?.access_token}`)
+
+    const url = `${process.env.API_BASE_URL}${routes.team.patch(organizationId, teamId)}`
+
+    const res = await fetch(url, {
       method: 'PATCH',
-      body: {
-        name,
-        description,
-        teamMembers: members,
-      },
+      headers,
+      body: JSON.stringify(data),
     })
 
     revalidateTag('teams')
 
-    return response
-  } catch (error: unknown) {
-    //@ts-expect-error: unknown server error type
-    return { error: error?.message || 'error_occurred_msg' }
-  }
-}
+    const jsonResponse = await res.json()
 
-export const getTeam = async (organizationId: string, teamId: string) => {
-  const session = await auth()
-  if (!session || !session.user.userId) {
-    return { error: 'unauthorized_request' }
-  }
+    if (jsonResponse?.errors && jsonResponse.errors?.length > 0) {
+      return { error: 'error_occurred_msg' }
+    }
 
-  try {
-    const response = await fetcher<Team, null>({
-      url: routes.team.getOne(organizationId, teamId),
-      method: 'GET',
-      revalidateTags: ['teams']
-    })
-
-    return response
-  } catch (_: unknown) {
+    return jsonResponse?.data ?? null
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    if (error?.message) {
+      return { error: error.message }
+    }
     return { error: 'error_occurred_msg' }
   }
 }
 
-export const deleteTeam = async (organizationId: string, teamId: string) => {
-  const session = await auth()
-  if (!session || !session.user.userId) {
-    return { error: 'unauthorized_request' }
-  }
-
+export async function getTeam(
+  organizationId: string,
+  teamId: string,
+): Promise<Team | { error: string }> {
   try {
-    const response = await fetcher<null, null>({
-      url: routes.team.delete(organizationId, teamId),
+    const session = await auth()
+    const headers = new Headers()
+
+    headers.append('Content-Type', 'application/json')
+    headers.append('Authorization', `Bearer ${session?.token?.access_token}`)
+
+    const url = `${process.env.API_BASE_URL}${routes.team.getOne(organizationId, teamId)}`
+    const res = await fetch(url, {
+      method: 'GET',
+      headers,
+      next: {
+        tags: ['teams'],
+      },
+    })
+
+    const jsonResponse = await res.json()
+
+    if (jsonResponse?.errors && jsonResponse.errors?.length > 0) {
+      return { error: 'error_occurred_msg' }
+    }
+
+    return jsonResponse?.data
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    if (error?.message) {
+      return { error: error.message }
+    }
+    return { error: 'error_occurred_msg' }
+  }
+}
+
+export async function deleteTeam(
+  organizationId: string,
+  teamId: string,
+): Promise<null | { error: string }> {
+  try {
+    const session = await auth()
+    const headers = new Headers()
+
+    headers.append('Content-Type', 'application/json')
+    headers.append('Authorization', `Bearer ${session?.token?.access_token}`)
+
+    const url = `${process.env.API_BASE_URL}${routes.team.delete(organizationId, teamId)}`
+    const res = await fetch(url, {
       method: 'DELETE',
+      headers,
     })
 
     revalidateTag('teams')
 
-    return response
-  } catch (_: unknown) {
+    const jsonResponse = await res.json()
+
+    if (jsonResponse?.errors && jsonResponse.errors?.length > 0) {
+      return { error: 'error_occurred_msg' }
+    }
+
+    return jsonResponse?.data ?? null
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    if (error?.message) {
+      return { error: error.message }
+    }
     return { error: 'error_occurred_msg' }
   }
 }
