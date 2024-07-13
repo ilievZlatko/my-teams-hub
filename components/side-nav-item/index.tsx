be1c98009c4ef085c6549bc2192ad729ca93c468
Zarko Boyukliev <122691228+ZBoyukliev@users.noreply.github.com>
2024-07-13 22:51:12 +0300

    Feat/mth 04 create layout (#25)

--- components/side-nav-item/index.tsx
+++ components/side-nav-item/index.tsx
@@ -1,68 +1,30 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '@radix-ui/react-accordion'
+import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 import Link from 'next/link'
-
-interface SubRoute {
-  routeName: string
-  subRoutes?: SubRoute[]
-  url?: string
-}
+import { SidenavKeys } from '@/enums/side-nav'
 
 interface SideNavItemProps {
   image: string
-  routeName: string
-  subRoutes?: SubRoute[]
+  routeName: SidenavKeys
   isOpen?: boolean
   url?: string
   onToggle?: () => void
   onRouteClick?: (url?: string) => void
 }
-
 const SideNavItem: React.FC<SideNavItemProps> = ({
   image,
   routeName,
-  subRoutes,
   isOpen,
   url,
   onToggle,
 }) => {
-  const [openAccordionItems, setOpenAccordionItems] = useState<string[]>([])
-
-  useEffect(() => {
-    if (isOpen && subRoutes) {
-      setOpenAccordionItems(
-        subRoutes.map((_, index) => `${routeName}-${index}`),
-      )
-    } else {
-      setOpenAccordionItems([])
-    }
-  }, [isOpen, routeName, subRoutes])
-
-  const toggleAccordionItem = (value: string) => {
-    setOpenAccordionItems((prevOpenItems) =>
-      prevOpenItems.includes(value)
-        ? prevOpenItems.filter((item) => item !== value)
-        : [...prevOpenItems, value],
-    )
-  }
-
-  const isAccordionItemOpen = (value: string) =>
-    openAccordionItems.includes(value)
+  const t = useTranslations('navigation.sidenav')
 
   return (
     <div className="flex w-full flex-col justify-between">
-      <div
-        className="flex w-full cursor-pointer items-center gap-6"
-        onClick={() => toggleAccordionItem(routeName)}
-      >
+      <div className="flex w-full cursor-pointer items-center gap-6">
         <Image
           src={image}
           alt="side-bar-image"
@@ -71,114 +33,14 @@ const SideNavItem: React.FC<SideNavItemProps> = ({
           onClick={onToggle}
         />
         <div className="flex w-44 justify-between">
-          {url ? (
-            <Link
-              href={url as string}
-              className={`mb-0 font-poppins text-base font-normal leading-20 text-mth-silver-200 transition duration-300 ease-in-out hover:text-mth-grey-blue-900 lg:text-20 ${isOpen ? 'block' : 'hidden'}`}
-            >
-              {routeName}
-            </Link>
-          ) : (
-            <p
-              className={`mb-0 font-poppins text-base font-normal leading-20 text-mth-silver-200 transition duration-300 ease-in-out hover:text-mth-grey-blue-900 lg:text-20 ${isOpen ? 'block' : 'hidden'}`}
-            >
-              {routeName}
-            </p>
-          )}
-
-          {subRoutes && isOpen && (
-            <div className="ml-3 pt-1">
-              <Image
-                src={
-                  isAccordionItemOpen(routeName)
-                    ? '/assets/images/arrowup.svg'
-                    : '/assets/images/arrowdown.svg'
-                }
-                alt={isAccordionItemOpen(routeName) ? 'arrowup' : 'arrowdown'}
-                width={16}
-                height={16}
-              />
-            </div>
-          )}
+          <Link
+            href={url as string}
+            className={`mb-0 whitespace-nowrap font-poppins text-base font-normal leading-20 text-mth-silver-200 transition duration-300 ease-in-out hover:text-mth-grey-blue-900 ${isOpen ? 'block' : 'hidden'}`}
+          >
+            {t(routeName)}
+          </Link>
         </div>
       </div>
-
-      {subRoutes && isOpen && (
-        <Accordion
-          type="multiple"
-          className={
-            isAccordionItemOpen(routeName) ? 'block w-full pt-5' : 'hidden'
-          }
-        >
-          {subRoutes.map((subRoute, index) => (
-            <AccordionItem
-              key={index}
-              value={`${routeName}-${index}`}
-              className="mb-6 w-full"
-            >
-              {/* {subRoute.url} */}
-              <AccordionTrigger
-                className="mb-3 ml-12 mr-4 flex w-44 justify-between gap-y-4 text-mth-silver-200 transition duration-300 ease-in-out hover:text-mth-grey-blue-900"
-                onClick={() => toggleAccordionItem(`${routeName}-${index}`)}
-              >
-                {subRoute.url ? (
-                  <Link
-                    className="text-base font-normal leading-20 lg:text-base"
-                    href={subRoute.url as string}
-                  >
-                    {subRoute.routeName}
-                  </Link>
-                ) : (
-                  <p className="text-base font-normal leading-20 lg:text-base">
-                    {subRoute.routeName}
-                  </p>
-                )}
-
-                {subRoute.subRoutes && (
-                  <Image
-                    src={
-                      isAccordionItemOpen(`${routeName}-${index}`)
-                        ? '/assets/images/arrowdown.svg'
-                        : '/assets/images/arrowup.svg'
-                    }
-                    alt={
-                      isAccordionItemOpen(`${routeName}-${index}`)
-                        ? 'arrowdown'
-                        : 'arrowup'
-                    }
-                    className="my-auto block"
-                    width={16}
-                    height={16}
-                  />
-                )}
-              </AccordionTrigger>
-
-              {subRoute.subRoutes && (
-                <AccordionContent>
-                  {subRoute.subRoutes.map((nestedSubRoute, nestedIndex) =>
-                    nestedSubRoute.url ? (
-                      <Link
-                        key={nestedIndex}
-                        className="mb-2 ml-8 block cursor-pointer pl-12 font-poppins text-sm font-normal leading-20 text-mth-silver-200 transition duration-300 ease-in-out hover:text-mth-grey-blue-900 lg:text-sm"
-                        href={nestedSubRoute.url as string}
-                      >
-                        {nestedSubRoute.routeName}
-                      </Link>
-                    ) : (
-                      <p
-                        key={nestedIndex}
-                        className="mb-2 ml-8 cursor-pointer pl-12 font-poppins text-sm font-normal leading-20 text-mth-silver-200 transition duration-300 ease-in-out hover:text-mth-grey-blue-900 lg:text-sm"
-                      >
-                        {nestedSubRoute.routeName}
-                      </p>
-                    ),
-                  )}
-                </AccordionContent>
-              )}
-            </AccordionItem>
-          ))}
-        </Accordion>
-      )}
     </div>
   )
 }
