9a665510813f9bfc338ef2d6f289fa80fdf0ed90
Zlatko <iliev.zlatko@gmail.com>
2024-07-19 17:18:03 +0300

    fetch the users

--- app/[locale]/(dashboard)/users/page.tsx
+++ app/[locale]/(dashboard)/users/page.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
-import { GetAllUsersComponent } from '@/components/list-users' 
+import { GetAllUsersComponent } from '@/components/list-users'
+import { getAllUsers } from '@/actions/user.actions'
+import { toast } from 'sonner'
 
-export default function Teams() {
-  return <GetAllUsersComponent />
+export default async function Teams() {
+  const users = await getAllUsers()
+
+  if ('error' in users) {
+    return toast.error(users.error)
+  }
+
+  return <GetAllUsersComponent users={users} />
 }
--- components/list-users/index.tsx
+++ components/list-users/index.tsx
@@ -1,287 +1,226 @@
 'use client'
 
-import { ChangeEvent, useEffect, useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import Image from 'next/image'
 import { useTranslations } from 'next-intl'
 import { Search, LayoutGrid, List, X } from 'lucide-react'
 import { useSession } from 'next-auth/react'
-
-import { Organisation } from '@/types/organisation.types'
-import { getAllUsers } from '@/actions/user.actions'
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from '@/components/ui/select'
 import {
-    Table,
-    TableBody,
-    TableHead,
-    TableHeader,
-    TableRow,
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from '@/components/ui/table'
 import { Input } from '@/components/ui/input'
 import { PaginationComponent } from '@/components/pagination'
 import { Loader } from '@/components/loader'
 import { cn } from '@/lib/utils'
-import { UserList } from '@/types/user'
+import { IUser } from '@/types/user'
+import { redirect } from 'next/navigation'
 
-export const GetAllUsersComponent = () => {
-    const t = useTranslations('page.user.index')
-    const tErrors = useTranslations('apierrors')
+interface GetAllUsersComponentProps {
+  users: IUser[]
+}
 
-    const { data: session, status } = useSession()
-    const [hasSession, setHasSession] = useState(false)
+export const GetAllUsersComponent = ({ users }: GetAllUsersComponentProps) => {
+  const t = useTranslations('page.user.index')
+  const tErrors = useTranslations('apierrors')
+  const rowsPerPage = 1
+  const [valueState, setValue] = useState(1)
+  const [userView, setuserView] = useState(false)
+  const [currentPage, setCurrentPage] = useState(rowsPerPage)
+  const [searchValue, setSearchValue] = useState('')
 
-    const [valueState, setValue] = useState(1)
-    const [userView, setuserView] = useState(false)
-    const [allUsers, setAllUsers] = useState<UserList | undefined>(undefined)
+  const { data: session, status } = useSession()
 
-    const [isFetchingData, setIsFetchingData] = useState(false)
-    const [currentOrganization, setCurrentOrganization] = useState<
-        Organisation | undefined
-    >(undefined)
+  function handleSearchChange(e: ChangeEvent<HTMLInputElement>) {
+    setSearchValue(e.target.value)
+  }
 
-    const rowsPerPage = 1
-    const [currentPage, setCurrentPage] = useState(rowsPerPage)
-    const [searchValue, setSearchValue] = useState('')
+  const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    useEffect(() => {
-        if (session && status === 'authenticated') setHasSession(true)
-    }, [session])
+  const valueChange = async (value: string) => {
+    setValue(Number(value))
+  }
 
-    useEffect(() => {
-        if (session?.user?.activeOrg) {
-            setCurrentOrganization(
-                () =>
-                    session.user.organizations.filter(
-                        (org: Organisation) =>
-                            org.organizationId === session.user.activeOrg,
-                    )[0],
-            )
+  const userViewChange = async () => {
+    setuserView(!userView)
+  }
 
-            fetchUsers()
-        }
-    }, [hasSession, session?.user.activeOrg])
+  const previousPage = () => {
+    setCurrentPage((currentPage) => currentPage - 1)
+  }
 
-    function fetchUsers() {
-        if (!session) return
+  const nextPage = () => {
+    setCurrentPage((currentPage) => currentPage + 1)
+  }
 
-        setIsFetchingData(true)
+  function handleDataFromChild(data: number) {
+    setCurrentPage(data)
+  }
 
-        getAllUsers()
-            .then((data) => {
-                console.log(data)
-                // if (Object.keys(data).find((key) => key === 'users')) {
-                //   setAllUsers(data as UserList)
-                // } else {
-                //   setAllUsers(undefined)
+  const countPages = users ? Math.ceil(users.length / valueState) : 1
 
-                //   toast.error(tErrors(data.error))
-                // }
-            })
-            .finally(() => {
-                setIsFetchingData(false)
-            })
-    }
+  if (status === 'loading') return <Loader size={44} className="m-auto" />
 
-    function handleSearchChange(e: ChangeEvent<HTMLInputElement>) {
-        setSearchValue(e.target.value)
-    }
+  if (!session) redirect('/login')
 
-    const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  return (
+    <div className="flex w-full flex-col lg:flex-row lg:gap-1 xl:max-w-[1100px]">
+      <Card className="flex w-full flex-col border-none bg-transparent shadow-none">
+        <CardHeader className="relative mb-1 flex w-full lg:mb-3">
+          <h1 className="text-center font-roboto text-[32px] font-normal leading-[38.4px] text-mth-grey-blue-700">
+            Team name
+          </h1>
+          <span className="relative left-[50%] max-w-[220px] translate-x-[-50%] rounded border" />
+          {userView ? (
+            <LayoutGrid
+              className="absolute right-2 top-[50%] translate-y-[-50%] cursor-pointer text-mth-blue-500"
+              onClick={userViewChange}
+            />
+          ) : (
+            <List
+              className="absolute right-2 top-[50%] translate-y-[-50%] cursor-pointer text-mth-blue-500"
+              onClick={userViewChange}
+            />
+          )}
+        </CardHeader>
 
-    const valueChange = async (value: string) => {
-        setValue(Number(value))
-    }
+        <CardContent
+          className={
+            userView === true ? 'rounded-[12px] max-sm:px-1' : 'max-sm:px-1'
+          }
+        >
+          <div className="mb-6 flex">
+            <div className="relative">
+              <Input
+                type="text"
+                placeholder={t('search_placeholder')}
+                className="form-input m-[10px] w-[300px] px-[36px] placeholder:text-xs max-sm:w-[240px]"
+                value={searchValue}
+                onChange={handleSearchChange}
+              />
+              <span className="absolute left-[20px] top-[20px]">
+                <Search className="size-5 select-none text-[#63929e] max-sm:size-4" />
+              </span>
+              <span className="absolute left-[280px] top-[20px] max-sm:left-[225px]">
+                <X
+                  className={cn(
+                    'size-5 cursor-pointer select-none text-[#63929e] max-sm:size-4',
+                    searchValue !== '' ? 'block' : 'hidden',
+                  )}
+                  onClick={() => setSearchValue('')}
+                />
+              </span>
+            </div>
+            <Image
+              src="/assets/images/vector.svg"
+              className="select-none"
+              alt="vector logo"
+              width={16}
+              height={16}
+              priority
+            />
+          </div>
 
-    const userViewChange = async () => {
-        setuserView(!userView)
-    }
-
-    const previousPage = () => {
-        setCurrentPage((currentPage) => currentPage - 1)
-    }
-
-    const nextPage = () => {
-        setCurrentPage((currentPage) => currentPage + 1)
-    }
-
-    function handleDataFromChild(data: number) {
-        setCurrentPage(data)
-    }
-
-    let firstUser = 0
-    if (currentPage !== 1) {
-        firstUser = valueState * (currentPage - 1)
-    }
-
-    const countPages =
-        allUsers !== undefined ? Math.ceil(allUsers?.total / valueState) : 1
-
-
-    if (!session || isFetchingData) return <Loader size={44} className="m-auto" />
-
-    return (
-        <div className="flex w-full flex-col lg:flex-row lg:gap-1 xl:max-w-[1100px]">
-            <Card className="flex w-full flex-col border-none bg-transparent shadow-none">
-                <CardHeader className="relative mb-1 flex w-full lg:mb-3">
-                    <h1 className="text-center font-roboto text-[32px] font-normal leading-[38.4px] text-mth-grey-blue-700">
-                        Team name
-                    </h1>
-                    <span className="relative left-[50%] max-w-[220px] translate-x-[-50%] rounded border" />
-                    {userView ? (
-                        <LayoutGrid
-                            className="absolute right-2 top-[50%] translate-y-[-50%] cursor-pointer text-mth-blue-500"
-                            onClick={userViewChange}
-                        />
-                    ) : (
-                        <List
-                            className="absolute right-2 top-[50%] translate-y-[-50%] cursor-pointer text-mth-blue-500"
-                            onClick={userViewChange}
-                        />
-                    )}
-                </CardHeader>
-
-                <CardContent
-                    className={
-                        userView === true ? 'rounded-[12px] max-sm:px-1' : 'max-sm:px-1'
-                    }
-                >
-                    <div className="mb-6 flex">
-                        <div className="relative">
-                            <Input
-                                type="text"
-                                placeholder={t('search_placeholder')}
-                                className="form-input m-[10px] w-[300px] px-[36px] placeholder:text-xs max-sm:w-[240px]"
-                                value={searchValue}
-                                onChange={handleSearchChange}
-                            />
-                            <span className="absolute left-[20px] top-[20px]">
-                                <Search className="size-5 select-none text-[#63929e] max-sm:size-4" />
-                            </span>
-                            <span className="absolute left-[280px] top-[20px] max-sm:left-[225px]">
-                                <X
-                                    className={cn(
-                                        'size-5 cursor-pointer select-none text-[#63929e] max-sm:size-4',
-                                        searchValue !== '' ? 'block' : 'hidden',
-                                    )}
-                                    onClick={() => setSearchValue('')}
-                                />
-                            </span>
-                        </div>
+          {userView ? (
+            <div className="rounded-xl border-[3px] border-mth-blue-100 px-1 py-10 lg:px-4">
+              <Table>
+                <TableHeader className="border-none">
+                  <TableRow className="items-center border-none">
+                    <TableHead>
+                      <span className="flex items-center gap-1 max-sm:items-start">
+                        {t('name')}
                         <Image
-                            src="/assets/images/vector.svg"
-                            className="select-none"
-                            alt="vector logo"
-                            width={16}
-                            height={16}
-                            priority
+                          src="/assets/images/up_and_down_arrow.svg"
+                          width={11}
+                          height={11}
+                          alt="arrow"
                         />
-                    </div>
+                      </span>
+                    </TableHead>
+                    <TableHead>
+                      <span className="flex items-center gap-1 max-sm:items-start">
+                        {t('team')}
+                        <Image
+                          src="/assets/images/up_and_down_arrow.svg"
+                          width={11}
+                          height={11}
+                          alt="arrow"
+                        />
+                      </span>
+                    </TableHead>
+                    <TableHead>
+                      <span className="flex items-center gap-1">
+                        {t('creation_date')}
+                        <Image
+                          src="/assets/images/up_and_down_arrow.svg"
+                          width={11}
+                          height={11}
+                          alt="arrow"
+                        />
+                      </span>
+                    </TableHead>
+                    <TableHead>
+                      <span className="flex items-center gap-1">
+                        {t('status')}
+                        <Image
+                          src="/assets/images/up_and_down_arrow.svg"
+                          width={11}
+                          height={11}
+                          alt="arrow"
+                          priority
+                        />
+                      </span>
+                    </TableHead>
+                    <TableHead>{t('action')}</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody></TableBody>
+              </Table>
+            </div>
+          ) : (
+            <div className="flex flex-col flex-wrap items-center gap-6 lg:flex-row lg:items-stretch lg:justify-center"></div>
+          )}
+        </CardContent>
 
-                    {userView ? (
-                        <div className="rounded-xl border-[3px] border-mth-blue-100 px-1 py-10 lg:px-4">
-                            <Table>
-                                <TableHeader className="border-none">
-                                    <TableRow className="items-center border-none">
-                                        <TableHead>
-                                            <span className="flex items-center gap-1 max-sm:items-start">
-                                                {t('name')}
-                                                <Image
-                                                    src="/assets/images/up_and_down_arrow.svg"
-                                                    width={11}
-                                                    height={11}
-                                                    alt="arrow"
-                                                />
-                                            </span>
-                                        </TableHead>
-                                        <TableHead>
-                                            <span className="flex items-center gap-1 max-sm:items-start">
-                                                {t('team')}
-                                                <Image
-                                                    src="/assets/images/up_and_down_arrow.svg"
-                                                    width={11}
-                                                    height={11}
-                                                    alt="arrow"
-                                                />
-                                            </span>
-                                        </TableHead>
-                                        <TableHead>
-                                            <span className="flex items-center gap-1">
-                                                {t('creation_date')}
-                                                <Image
-                                                    src="/assets/images/up_and_down_arrow.svg"
-                                                    width={11}
-                                                    height={11}
-                                                    alt="arrow"
-                                                />
-                                            </span>
-                                        </TableHead>
-                                        <TableHead>
-                                            <span className="flex items-center gap-1">
-                                                {t('status')}
-                                                <Image
-                                                    src="/assets/images/up_and_down_arrow.svg"
-                                                    width={11}
-                                                    height={11}
-                                                    alt="arrow"
-                                                    priority
-                                                />
-                                            </span>
-                                        </TableHead>
-                                        <TableHead>{t('action')}</TableHead>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
-
-                                // TODO: List users in table format
-
-                                </TableBody>
-                            </Table>
-                        </div>
-                    ) : (
-                        <div className="flex flex-col flex-wrap items-center gap-6 lg:flex-row lg:items-stretch lg:justify-center">
-
-                            // TODO: List users
-
-                        </div>
-                    )}
-
-                </CardContent>
-
-                <div className="mt-[16px] flex items-center justify-end gap-[10px] text-xs">
-                    <p>{t('show')}</p>
-                    <div className="w-[60px]">
-                        <Select onValueChange={valueChange}>
-                            <SelectTrigger className="w-full bg-transparent">
-                                <SelectValue placeholder={valueState} />
-                            </SelectTrigger>
-                            <SelectContent>
-                                {pages?.map((page) => (
-                                    <SelectItem key={page} value={page.toString()}>
-                                        {page}
-                                    </SelectItem>
-                                ))}
-                            </SelectContent>
-                        </Select>
-                    </div>
-                </div>
-
-                <CardFooter className="mt-[50px]">
-                    <PaginationComponent
-                        nextPage={nextPage}
-                        previousPage={previousPage}
-                        sendCurrentPage={handleDataFromChild}
-                        currentPage={currentPage}
-                        countPages={countPages}
-                    />
-                </CardFooter>
-            </Card>
+        <div className="mt-[16px] flex items-center justify-end gap-[10px] text-xs">
+          <p>{t('show')}</p>
+          <div className="w-[60px]">
+            <Select onValueChange={valueChange}>
+              <SelectTrigger className="w-full bg-transparent">
+                <SelectValue placeholder={valueState} />
+              </SelectTrigger>
+              <SelectContent>
+                {pages?.map((page) => (
+                  <SelectItem key={page} value={page.toString()}>
+                    {page}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
         </div>
 
-    )
+        <CardFooter className="mt-[50px]">
+          <PaginationComponent
+            nextPage={nextPage}
+            previousPage={previousPage}
+            sendCurrentPage={handleDataFromChild}
+            currentPage={currentPage}
+            countPages={countPages}
+          />
+        </CardFooter>
+      </Card>
+    </div>
+  )
 }
