25272b93bd4ceef8b6fc2b534c45f16f43af54b6
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-07-23 12:47:45 +0300

    format the code

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -15,10 +15,7 @@ export default function Header({ name, email }: HeaderProps) {
   return (
     <header className="w-full bg-mth-grey-blue-600">
       <nav className="container flex items-center justify-between px-4 py-3 lg:px-8">
-        <ResponsiveSideNav
-          name={name}
-          email={email}
-        />
+        <ResponsiveSideNav name={name} email={email} />
         <div className="flex w-72 items-center justify-between gap-x-4 lg:ml-auto lg:w-80">
           <OrganizationSwitcher />
           <div className="items-centers flex justify-center px-2">
@@ -26,9 +23,7 @@ export default function Header({ name, email }: HeaderProps) {
             <SelectLocale />
           </div>
           <div className="hidden lg:block">
-            <UserMenu 
-              name={name}
-              email={email} />
+            <UserMenu name={name} email={email} />
           </div>
         </div>
       </nav>
